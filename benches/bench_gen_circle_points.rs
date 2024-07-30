e18d48f9815bdbd1741ed1c8fc7430f304488e00
Karl Wulfert <pic16f877.ccs@emailn.de>
2024-07-30 10:30:18 +0200

    Add tests and bench for f32

--- benches/bench_gen_circle_points.rs
+++ benches/bench_gen_circle_points.rs
@@ -1,15 +1,25 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use iterextd::GenCirclePoints;
 
-fn bench_create_circle_points(c: &mut Criterion) {
-    c.bench_function("Bench polar points", move |b| {
+fn bench_create_circle_points_f32(c: &mut Criterion) {
+    c.bench_function("Bench polar points f32", move |b| {
         b.iter(|| {
-            for elem in black_box(GenCirclePoints::new(5.0, 1034)) {
+            for elem in black_box(GenCirclePoints::new(5.0_f32, 1034)) {
                 black_box(elem);
             }
         })
     });
 }
 
-criterion_group!(benches, bench_create_circle_points);
+fn bench_create_circle_points_f64(c: &mut Criterion) {
+    c.bench_function("Bench polar points f64", move |b| {
+        b.iter(|| {
+            for elem in black_box(GenCirclePoints::new(5.0_f64, 1034)) {
+                black_box(elem);
+            }
+        })
+    });
+}
+
+criterion_group!(benches, bench_create_circle_points_f32,  bench_create_circle_points_f64);
 criterion_main!(benches);
--- tests/test_gen_circle_points.rs
+++ tests/test_gen_circle_points.rs
@@ -2,8 +2,77 @@ use approx::assert_relative_eq;
 use iterextd::GenCirclePoints;
 
 #[test]
+fn test_gen_circle_points_debug_f32() {
+    let iter = GenCirclePoints::new(1.0_f32, 0);
+    assert_eq!(
+        format!("{:?}", iter),
+        "GenCirclePoints { radius: 1.0, num_points: 0, \
+            base_cos: 1.0, base_sin: 0.0, incr_cos: NaN, incr_sin: NaN }"
+    );
+}
+
+#[test]
+fn test_gen_circle_points_len_f32() {
+    let mut iter = GenCirclePoints::new(1.0_f32, 6);
+    assert_eq!(iter.size_hint(), (6, Some(6)));
+    assert_eq!(iter.len(), 6);
+    iter.next();
+    assert_eq!(iter.size_hint(), (5, Some(5)));
+    assert_eq!(iter.len(), 5);
+    iter.nth(4);
+    assert_eq!(iter.size_hint(), (0, Some(0)));
+    assert_eq!(iter.len(), 0);
+    iter.next();
+    assert_eq!(iter.size_hint(), (0, Some(0)));
+    assert_eq!(iter.len(), 0);
+}
+
+#[test]
+fn test_gen_circle_points_point_f32() {
+    let iter = GenCirclePoints::new(1.0_f32, 1);
+    let iter_clone = iter.clone();
+    assert_eq!(iter.size_hint(), (1, Some(1)));
+    assert_eq!(iter.len(), 1);
+    let vec = iter.collect::<Vec<_>>();
+    assert_relative_eq!(vec[0].0, 1.0e0, epsilon = 1.0e-16);
+    assert_relative_eq!(vec[0].1, 0.0e0, epsilon = 1.0e-16);
+    let vec_clone = iter_clone.collect::<Vec<_>>();
+    assert_relative_eq!(vec_clone[0].0, 1.0e0, epsilon = 1.0e-16);
+    assert_relative_eq!(vec_clone[0].1, 0.0e0, epsilon = 1.0e-16);
+}
+
+#[test]
+fn test_gen_circle_points_three_points_f32() {
+    let iter = GenCirclePoints::new(1.0_f32, 3);
+    let vec = iter.collect::<Vec<_>>();
+    let cos_0_deg = 1.0e0_f32;
+    let sin_0_deg = 0.0e0_f32;
+    let cos_120_deg = -0.5e0_f32;
+    let sin_120_deg = 0.8660254037844386_f32;
+    let cos_240_deg = -0.5e0_f32;
+    let sin_240_deg = -0.8660254037844386_f32;
+    assert_relative_eq!(vec[0].0, cos_0_deg, epsilon = 1.0e-8);
+    assert_relative_eq!(vec[0].1, sin_0_deg, epsilon = 1.0e-8);
+    assert_relative_eq!(vec[1].0, cos_120_deg, epsilon = 2.0e-7);
+    assert_relative_eq!(vec[1].1, sin_120_deg, epsilon = 2.0e-7);
+    assert_relative_eq!(vec[2].0, cos_240_deg, epsilon = 2.0e-7);
+    assert_relative_eq!(vec[2].1, sin_240_deg, epsilon = 2.0e-7);
+}
+
+#[test]
+fn test_gen_circle_points_points_f32() {
+    let num_points = 739;
+    let mut iter = GenCirclePoints::new(1.0_f32, num_points);
+    let (cos, sin) = iter.nth(num_points - 1).unwrap();
+    let rhs_cos = 0.9999638558306818_f32;
+    assert_relative_eq!(cos, rhs_cos, epsilon = 2.0e-7);
+    let rhs_sin = -0.00850217808772121_f32;
+    assert_relative_eq!(sin, rhs_sin, epsilon = 2.0e-7);
+}
+
+#[test]
 fn test_gen_circle_points_debug() {
-    let iter = GenCirclePoints::new(1.0, 0);
+    let iter = GenCirclePoints::new(1.0_f64, 0);
     assert_eq!(
         format!("{:?}", iter),
         "GenCirclePoints { radius: 1.0, num_points: 0, \
@@ -13,7 +82,7 @@ fn test_gen_circle_points_debug() {
 
 #[test]
 fn test_gen_circle_points_len() {
-    let mut iter = GenCirclePoints::new(1.0, 6);
+    let mut iter = GenCirclePoints::new(1.0_f64, 6);
     assert_eq!(iter.size_hint(), (6, Some(6)));
     assert_eq!(iter.len(), 6);
     iter.next();
@@ -29,7 +98,7 @@ fn test_gen_circle_points_len() {
 
 #[test]
 fn test_gen_circle_points_point() {
-    let iter = GenCirclePoints::new(1.0, 1);
+    let iter = GenCirclePoints::new(1.0_f64, 1);
     let iter_clone = iter.clone();
     assert_eq!(iter.size_hint(), (1, Some(1)));
     assert_eq!(iter.len(), 1);
@@ -43,7 +112,7 @@ fn test_gen_circle_points_point() {
 
 #[test]
 fn test_gen_circle_points_three_points() {
-    let iter = GenCirclePoints::new(1.0, 3);
+    let iter = GenCirclePoints::new(1.0_f64, 3);
     let vec = iter.collect::<Vec<_>>();
     let cos_0_deg = 1.0e0_f64;
     let sin_0_deg = 0.0e0_f64;
@@ -62,7 +131,7 @@ fn test_gen_circle_points_three_points() {
 #[test]
 fn test_gen_circle_points_points() {
     let num_points = 739;
-    let mut iter = GenCirclePoints::new(1.0, num_points);
+    let mut iter = GenCirclePoints::new(1.0_f64, num_points);
     let (cos, sin) = iter.nth(num_points - 1).unwrap();
     let rhs_cos = 0.9999638558306818_f64;
     assert_relative_eq!(cos, rhs_cos, epsilon = 9.0e-16);
